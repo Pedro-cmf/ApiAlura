909e7c3d0b460df4ae70d66679f3394d51111acc
Pedro César <pedroocesarmf@gmail.com>
2024-06-12 16:20:35 -0300

    Inicializando projeto api rest

---
+++ src/main/java/Paciente/DadosCadastroPaciente.java
@@ -0,0 +1,12 @@
+package Paciente;
+
+import med.voll.api.Endereco.DadosEndereco;
+
+public record DadosCadastroPaciente(
+        String nome,
+        String email,
+        String telefone,
+        String cpf,
+        DadosEndereco endereco
+) {
+}
